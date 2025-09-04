93d576653fbd7e625fdbe57eb4d7d698ba6f2ad9
Francisco Mateos <frantmateos@gmail.com>
2025-09-04 17:22:06 -0300

    update: push TP3

--- TP3/shader/fragment.glsl
+++ TP3/shader/fragment.glsl
@@ -1,9 +1,86 @@
-#version 330 
+#version 330
 
-in vec3 v_color;
+out vec4 fragColor;
 
-out vec4 f_color;
+uniform float u_time;       // iempo
+uniform vec2 u_resolution;  // tamaño ventana
+uniform vec2 u_mouse;       // posicion mouse
+uniform int u_mode;         // teclado
 
-void main(){
-    f_color = vec4(v_color,1.0);
+vec3 random3(vec3 c) {
+    float j = 4096.0 * sin(dot(c, vec3(17.0, 59.4, 15.0)));
+    vec3 r;
+    r.z = fract(512.0 * j);
+    j *= .125;
+    r.x = fract(512.0 * j);
+    j *= .125;
+    r.y = fract(512.0 * j);
+    return r - 0.5;
+}
+
+const float F3 = 0.3333333;
+const float G3 = 0.1666667;
+
+float simplex3d(vec3 p) {
+    vec3 s = floor(p + dot(p, vec3(F3)));
+    vec3 x = p - s + dot(s, vec3(G3));
+    vec3 e = step(vec3(0.0), x - x.yzx);
+    vec3 i1 = e * (1.0 - e.zxy);
+    vec3 i2 = 1.0 - e.zxy * (1.0 - e);
+    vec3 x1 = x - i1 + G3;
+    vec3 x2 = x - i2 + 2.0 * G3;
+    vec3 x3 = x - 1.0 + 3.0 * G3;
+    vec4 w, d;
+    w.x = dot(x, x);
+    w.y = dot(x1, x1);
+    w.z = dot(x2, x2);
+    w.w = dot(x3, x3);
+    w = max(0.6 - w, 0.0);
+    d.x = dot(random3(s), x);
+    d.y = dot(random3(s + i1), x1);
+    d.z = dot(random3(s + i2), x2);
+    d.w = dot(random3(s + 1.0), x3);
+    w *= w; w *= w;
+    d *= w;
+    return dot(d, vec4(52.0));
+}
+
+float noise(vec3 m) {
+    return 0.5333333 * simplex3d(m)
+         + 0.2666667 * simplex3d(2.0 * m)
+         + 0.1333333 * simplex3d(4.0 * m)
+         + 0.0666667 * simplex3d(8.0 * m);
+}
+
+void main() {
+    vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+    uv = uv * 2.0 - 1.0;
+
+    vec2 p = gl_FragCoord.xy / u_resolution.x;
+    vec3 p3 = vec3(p, u_time * 0.4);
+
+    float intensity = noise(p3 * 8.0 + 12.0);
+
+    float t = clamp((uv.x * -uv.x * 0.16) + 0.15, 0., 1.);
+    float y = abs(intensity * -t + uv.y);
+
+    float g = pow(y, 0.2);
+
+    // color base
+    vec3 col = vec3(1.25, 1.48, 1.78);
+    col = col * -g + col;
+    col = col * col;
+    col = col * col;
+
+    // === Interacciones ===
+    if (u_mode == 1) {
+        // cambair fondo con "2"
+        col = vec3(intensity, 0.4, 0.2);
+    }
+    if (u_mode == 2) {
+        // ruido con la posición del mouse
+        col *= vec3(3.0 - u_mouse.x, 1.0 - u_mouse.y, 7.0);
+    }
+
+    fragColor = vec4(col, 1.0);
 }
--- TP3/tp3.py
+++ TP3/tp3.py
@@ -1,11 +1,15 @@
+import time
 import pyglet
 import moderngl
 import numpy as np
 from pathlib import Path
+import time
 
 class window(pyglet.window.Window):
     def __init__(self):
-        super().__init__(640, 400, caption="TP3 - Mateos")
+        self.mouse_pos = (0.0, 0.0)
+        self.mode = 0
+        super().__init__(1289, 720, caption="TP3 - Mateos")
         self.ctx = moderngl.create_context()
 
         shader_dir = Path(__file__).parent/"shader"
@@ -16,24 +20,41 @@ class window(pyglet.window.Window):
 
         self.prog = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
 
-        quad_points = [(0.5, 0.5), (-0.5, 0.5), (-0.5, -0.5), (0.5, -0.5)]
+        quad_points = [(1.0, 1.0), (-1.0, 1.0), (-1.0, -1.0), (1.0, -1.0)]
         vertices = []
-
-        r,g,b = (1.0, 1.0, 1.0)
-
         for (px,py) in quad_points:
-            vertices.extend([px, py, r, g, b])
+            vertices.extend([px, py, 1.0, 1.0, 1.0])
 
         vertices_array = np.array(vertices, dtype='f4')
         vbo = self.ctx.buffer(vertices_array.tobytes())
+        self.vao = self.ctx.vertex_array(self.prog, [(vbo,"2f 3f", "in_pos", "in_color")])
+        self.start = time.time()
 
-        self.vao = self.ctx.vertex_array(self.prog,[(vbo,"2f 3f", "in_pos", "in_color")])
-    
     def on_draw(self):
         self.clear()
-        self.ctx.clear(0.0,0.0,0.0)
-        self.vao.render(mode = moderngl.TRIANGLE_FAN)
+        self.ctx.clear(0.0, 0.0, 0.0)
+
+        self.prog["u_time"].value = time.time() - self.start
+        self.prog["u_resolution"].value = (self.width, self.height)
+
+        self.prog["u_mouse"].value = self.mouse_pos
+        self.prog["u_mode"].value = self.mode
+
+        self.vao.render(mode=moderngl.TRIANGLE_FAN)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_pos = (x / self.width, y / self.height)
+
+    def on_key_press(self, symbol, modifiers):
+        # cambiar modo con teclas 1,2,3
+        if symbol == pyglet.window.key._1:
+            self.mode = 0
+        elif symbol == pyglet.window.key._2:
+            self.mode = 1
+        elif symbol == pyglet.window.key._3:
+            self.mode = 2
 
 
-window()
-pyglet.app.run()
+if __name__ == "__main__":
+    window()
+    pyglet.app.run()
